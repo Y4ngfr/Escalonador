778a1a0d060cc7c22bf148c991494afe417cf88c
Y4ngfr <yangrodrigues.aluno@unipampa.edu.br>
2024-10-08 18:12:48 -0300

    att readme

--- src/models/despachante.py
+++ src/models/despachante.py
@@ -7,12 +7,12 @@ class Despachante:
         self.tempo = 0
 
     def executar_processo(self, processo):
-        if random.randint(1, 100) <= 30:
-            processo.setEstado("ESPERANDO")
-            processo.tempo_espera = random.randint(1, 3)
-            self.tempo = 0
-        else:
-            processo.setEstado("EXECUTANDO")
+        # if random.randint(1, 100) <= 30:
+        #     processo.setEstado("ESPERANDO")
+        #     processo.tempo_espera = random.randint(1, 3)
+        #     self.tempo = 0
+        # else:
+        processo.setEstado("EXECUTANDO")
 
         if(  
             processo.getEstado() == "EXECUTANDO" and
